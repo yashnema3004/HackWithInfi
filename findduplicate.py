87494b3fa7c93df09533abc484d59b87e2f4a120
Yash Nema <yashnema30.04@gmail.com>
2026-03-15 16:55:23 +0530

    findduplicate.py

--- findduplicate.py
+++ findduplicate.py
@@ -5,6 +5,20 @@ class Solution:
         for i in range(1,n):
             if nums[i] == nums[i-1]:
                 return nums[i]
+
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        seen = {}
+        for i in range(len(nums)):
+            if nums[i] in seen:
+                return nums[i]
+            else:
+                seen[nums[i]]  = True
+
+                   
+
+        
             
 
         
